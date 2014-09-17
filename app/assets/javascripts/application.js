d20ae2360080ff63955989c30d42519b47275ca5
Kevin Powell <kevin@eightshapes.com>
2014-09-17 09:22:55 -0500

    Add git commit helper shortcut

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui/sortable
+//= require jquery.hotkeys
 //= require turbolinks
 // Loads all Bootstrap javascripts
 //= require bootstrap
