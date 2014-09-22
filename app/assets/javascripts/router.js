cbfca11dfc0d46a3d07e5b28beba947b197597b9
Kevin Powell <kevin@eightshapes.com>
2014-09-22 13:58:24 -0500

    Clean up unused code and restructure templates

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -4,18 +4,10 @@ Houston.Router.reopen({
 
 Houston.Router.map(function() {
 	this.resource('issues', { path: '/'}, function () {
-		// Additional child routes will go here later
-		// this.route('yahoo');
 		this.route('by-client', {path: ':slug/client/:client_name'});
 	});
 
 });
-
-// Houston.IssuesRoute = Ember.Route.extend({
-// 	model: function() {
-// 		return this.store.find('issue');
-// 	}
-// });
 
 Houston.IssuesIndexRoute = Ember.Route.extend({
 	model: function() {
