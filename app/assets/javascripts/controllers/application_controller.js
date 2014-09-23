e98ddf3da9be2e28f287c5afc420cb83045f8dd9
Kevin Powell <kevin@eightshapes.com>
2014-09-23 14:11:46 -0500

    Styling of sync progress bar

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -7,11 +7,11 @@ Houston.ApplicationController = Ember.ObjectController.extend(EmberPusher.Bindin
 	issues_synchronized: false,
 	progress_bar_width: "width:0;",
 	github_sync_complete: false,
+	syncing: false,
 	actions: {
 		githubIssueSyncCount: function(data) {
 			this.issues_to_be_synced = data.total_issues;
 			this.issues_synchronized = 0;
-			this.set('github_sync_complete', false);
 
 			if (this.issues_synchronized == this.issues_to_be_synced) {
 				alert("NOTHING TO SYNC");
@@ -24,14 +24,19 @@ Houston.ApplicationController = Ember.ObjectController.extend(EmberPusher.Bindin
 				this.get("controllers.issuesIndex").send("githubSyncComplete");
 				this.set('progress_bar_width', 'width:100%;');
 				this.set('github_sync_complete', true);
+				this.set('syncing', false);
 			}
 			else {
 				var width = ( this.issues_synchronized / this.issues_to_be_synced ) * 100;
 				this.set('progress_bar_width', 'width:' + width + '%;');
+				this.set('syncing', true);
 			}
-			console.log(this.progress_bar_width);
 		},
 	    syncGithubIssues: function() {
+			this.set('progress_bar_width', 'width:0;');
+			this.set('syncing', true);
+			this.set('github_sync_complete', false);
+
 			$.ajax({
 				url:'/synchronize-github-issues-for-user/' + current_user_id,
 				type: 'PUT'
